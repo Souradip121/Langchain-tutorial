d1859f5d4f37902a72e573176f0271ff165cb800
souradip121 <souradip12b@gmail.com>
2025-01-24 17:12:19 +0530

    Update main.py to modify prompt context and change invocation input; add requirements.txt for dependencies

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI()
 
-print(llm.invoke("how can langsmith help with testing?"))
+#print(llm.invoke("how can you subscribe our channel"))
 
 from langchain_core.prompts import ChatPromptTemplate
+
 prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a world class technical documentation writer."),
-    ("user", "{input}")
-])
+    ("system", "You are a great video audience"),
+    ("user","{input}")
+]   
+)
 
 chain = prompt | llm 
 
-chain.invoke({"input": "how can langsmith help with testing?"})
+#print(chain.invoke({"input":"how can you subscribe our channel"}))
 
 from langchain_core.output_parsers import StrOutputParser
 
@@ -20,7 +22,4 @@ output_parser = StrOutputParser()
 
 chain = prompt | llm | output_parser
 
-print(chain.invoke({"input": "how can langsmith help with testing?"}))
-
-
-
+print(chain.invoke({"input":"subscribe our channel"}))
